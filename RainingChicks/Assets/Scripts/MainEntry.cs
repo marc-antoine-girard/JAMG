0d83ee29f263f028d687e37a47cda2032f0a54dd
Gab_Desg <unconfigured@null.spigotmc.org>
2020-08-17 17:04:33 -0400

    MainEntry basic setup
    Added MainEntry to Menu scene

--- RainingChicks/Assets/Scripts/MainEntry.cs
+++ RainingChicks/Assets/Scripts/MainEntry.cs
@@ -1,18 +1,38 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainEntry : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+#region
+
+    public static MainEntry Instance = null;
+
+    private void Awake() {
+        if (Instance == null) {
+            Instance = this;
+            //GameObject.DontDestroyOnLoad(gameObject);
+        }
+        else {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+#endregion
+
+    private void Start() { }
+
+    private void Update() { }
+
+    private void FixedUpdate() { }
+
+    private void LateUpdate() { }
+
+    private void OnApplicationFocus(bool hasFocus) { }
+
+    private void OnApplicationPause(bool pauseStatus) { }
+
+    private void OnApplicationQuit() { }
 }
