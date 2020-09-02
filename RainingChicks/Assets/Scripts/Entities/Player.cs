3f55d21355d7e5b17bef1d06a5210611630ae2f6
Gab_Desg <unconfigured@null.spigotmc.org>
2020-09-01 22:23:45 -0400

    Class Player
    Initialize Rewired input for each player

---
+++ RainingChicks/Assets/Scripts/Entities/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Rewired;
+using UnityEngine;
+
+namespace Game
+{
+    public class Player : MonoBehaviour
+    {
+        private static ushort rewiredPlayerId = 0;
+
+        private static ushort GetNextRewiredPlayerId {
+            get {
+                if (rewiredPlayerId >= GameSettingsDev.AmountPlayerPerGameMax)
+                    rewiredPlayerId = 0;
+                return rewiredPlayerId++;
+            }
+
+            set => rewiredPlayerId = value;
+        }
+
+        private Rewired.Player inputs;
+
+        public void PreInitialize() {
+            inputs = ReInput.players.GetPlayer(GetNextRewiredPlayerId);
+        }
+
+        public void Initialize() { }
+
+        public void Refresh() { }
+
+        public void PhysicRefresh() { }
+    }
+}
